2b3f875522638417f513cf560eda4283082704b0
plumcraft <olivier.lebhard@gmail.com>
2017-01-29 12:47:22 +0100

    Add config file

---
+++ config.js
@@ -0,0 +1,1 @@
+var netscan     = { module: 'mmm-networkscanner', position: 'top_right', config: { devices: [ ENV_NET_DEVICES ], showUnknown: true, showOffline: true, keepAlive: 300, updateInterval: 25 }, classes: 'everyone' };
