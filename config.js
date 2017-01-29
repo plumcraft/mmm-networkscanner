1d24902a2138d7ea37f32e5a29f15695c155c48c
plumcraft <olivier.lebhard@gmail.com>
2017-01-29 17:09:53 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,1 @@
-var netscan     = { module: 'mmm-networkscanner', position: 'top_right', config: { devices: [ ENV_NET_DEVICES ], showUnknown: true, showOffline: true, keepAlive: 300, updateInterval: 25 }, classes: 'everyone' };
+{ module: 'mmm-networkscanner', position: 'top_right', config: { devices: [ ENV_NET_DEVICES ], showUnknown: true, showOffline: true, keepAlive: 300, updateInterval: 25 }, classes: 'everyone' };
